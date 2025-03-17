27963b5749d06b4163c74fdc74447582c89acb57
IndianTechSupp <90865605+IndianTechSupp7@users.noreply.github.com>
2025-03-17 22:04:01 +0100

    Update sort.py

--- src/assets/gym_images/equipment/sort.py
+++ src/assets/gym_images/equipment/sort.py
@@ -3,8 +3,6 @@ from encodings import ptcp154
 import enum
 import os
 
-from sympy import im
-
 imgs = [i for i in os.listdir("./") if i.split(".")[-1] in ["jpg", "png"]]
 for i, path in enumerate(imgs):
     os.rename(path, f"t_{i}.jpg")
