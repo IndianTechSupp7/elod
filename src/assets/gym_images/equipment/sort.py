556c52bf8055961c3f8907782272b6150abbb299
IndianTechSupp <90865605+IndianTechSupp7@users.noreply.github.com>
2025-03-17 22:04:19 +0100

    Update sort.py

--- src/assets/gym_images/equipment/sort.py
+++ src/assets/gym_images/equipment/sort.py
@@ -1,6 +1,4 @@
 # ez a script csak egy kis segítség hogy a galérába a képeket egyszerűbb legyen beimportálni
-from encodings import ptcp154
-import enum
 import os
 
 imgs = [i for i in os.listdir("./") if i.split(".")[-1] in ["jpg", "png"]]
